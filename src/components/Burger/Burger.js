48a91326955bc877f24aaa527409af20561a6933
Brian <brnjin6@gmail.com>
2019-07-19 16:06:33 -0400

    add ingredient function

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,37 +1,27 @@
 import React from 'react';
-import classes from './BurgerIngredient.module.css';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burgerIngredient = ( props ) => {
-    let ingredient = null;
-    
-    switch ( props.type ) {
+const burger = ( props ) => {
+    let transfromedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
+    if (transfromedIngredients.length === 0) {
+        transfromedIngredients = <p> Please add ingredients</p>;
+    }
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+                {transfromedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+}
 
-        case ('bread-bottom'):
-            ingredient = <div className={classes.BreadBottom}></div>;
-            break;
-        case ('bread-top'):
-            ingredient = (
-                <div className={classes.BreadTop}>
-                    <div className={classes.Seeds1}></div>
-                    <div className={classes.Seeds2}></div>
-                </div>);
-            break;
-        case ('meat'):
-            ingredient = <div className={classes.Meat}></div>
-            break;
-        case ('cheese'):
-            ingredient = <div className={classes.Cheese}></div>
-            break;
-        case ('salad'):
-            ingredient = <div className={classes.Salad} ></div>
-            break;
-        case ('bacon'):
-            ingredient = <div className={classes.Bacon}></div>
-            break;
-        default:
-            ingredient = null;
-        }
-    return ingredient;
-};
-
-export default burgerIngredient;
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import Auxillary from '../../hoc/Auxillary';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salda: 0.5,
+    cheese: 1,
+    meat: 1.5,
+    bacon: 1
+}
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = ( type ) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = ( type ) => {
+
+    }
     render() {
         return (
             <Auxillary>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <Burger ingredients = {this.state.ingredients}/>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                />
             </Auxillary>
             
         );
