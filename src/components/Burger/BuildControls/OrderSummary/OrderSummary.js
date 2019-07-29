1ee680692f5a02d6b208b70387df9e13bcf0c81a
Brian <brnjin6@gmail.com>
2019-07-29 15:19:07 -0400

    Modal backdrop fix and added life cycle

--- src/components/Burger/BuildControls/OrderSummary/OrderSummary.js
+++ src/components/Burger/BuildControls/OrderSummary/OrderSummary.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxillary from '../../../../hoc/Auxillary';
 import Button from '../../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log('[OrderSummary willUpdate');
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
-            return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+            return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
         });
 
-    return (
-        <Auxillary>
-            <h3> Your Order</h3>
-            <p>A delicious burger</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <strong>Total Price: ${props.price}</strong>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Auxillary>
-    )
+        return (
+            <Auxillary>
+                <h3> Your Order</h3>
+                <p>A delicious burger</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <strong>Total Price: ${this.props.price}</strong>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Auxillary>
+        )
+    }
+    
     
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Auxillary>
-        <Toolbar />
-        <SideDrawer />
-        <div>SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxillary>
-);
 
-export default layout;
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        } );
+    }
+
+    render() {
+        return (
+            <Auxillary>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxillary>
+        )
+    }
+};
+
+export default Layout;
